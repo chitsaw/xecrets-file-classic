38f6eaaad53de46bda7c0cbcd91bc3ea3dd62227
Nick Saw <chitsaw@microsoft.com>
2025-08-26 08:58:10 +0800

    Added ARM64 builds

--- Build.cmd
+++ Build.cmd
@@ -1,6 +1,19 @@
+@echo off
+setlocal
+
+rem Initialize x86 environment and build Win32
 call "%VSINSTALLDIR%vc\Auxiliary\Build\vcvarsall" x86
-MSBuild XecretsFileClassic.sln /p:Configuration=Debug;Platform=Win32
-MSBuild XecretsFileClassic.sln /p:Configuration=Release;Platform=Win32
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Debug;Platform=Win32
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Release;Platform=Win32
+
+rem Initialize amd64 environment and build x64
 call "%VSINSTALLDIR%vc\Auxiliary\Build\vcvarsall" amd64
-MSBuild XecretsFileClassic.sln /p:Configuration=Debug;Platform=x64
-MSBuild XecretsFileClassic.sln /p:Configuration=Release;Platform=x64
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Debug;Platform=x64
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Release;Platform=x64
+
+rem Initialize cross environment and build ARM64 (x64 host -> ARM64 target)
+call "%VSINSTALLDIR%vc\Auxiliary\Build\vcvarsall" amd64_arm64
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Debug;Platform=ARM64
+MSBuild XecretsFileClassic.sln /m /p:Configuration=Release;Platform=ARM64
+
+endlocal
